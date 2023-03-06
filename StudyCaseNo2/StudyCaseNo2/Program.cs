1763d1168b41ff2f41b4bcf8ebdcad75cd0d786b
rahma <103886606+rhmau1@users.noreply.github.com>
2023-03-06 14:48:33 +0700

    Example 2

---
+++ StudyCaseNo2/StudyCaseNo2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCaseNo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "2. Chatbot ini termasuk ke dalam Large Language Model (LLM), yaitu program komputer yang dapat (mengenali), (meringkas), (menerjemahkan), (memprediksi), dan (menghasilkan teks)";
+            Console.WriteLine(message); 
+            Console.WriteLine("Output:");
+
+            while (true)
+            {
+                int openingPosition = message.IndexOf('(');
+                if (openingPosition == -1) break;
+
+                openingPosition += 1;
+                int closingPosition = message.IndexOf(')');
+                int length = closingPosition - openingPosition;
+                Console.WriteLine($"-{message.Substring(openingPosition, length)}");
+                
+                message = message.Substring(closingPosition + 1);
+            }
+        }
+    }
+}
